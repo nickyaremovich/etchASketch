bb7dbcb2555b4a85247cd9a69609b5f95b70c415
Nick Yaremovich <nickyaremovich@gmail.com>
2022-06-09 16:45:48 -0600

    added mouse over functionality

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 function genBoard(size) { 
     let board = document.querySelector('.board');
-    board.style.gridTemplateColumns = `repeat(${size}), 1fr)`;
-    board.style.gridTemplateRows = `repeat(${size}), 1fr)`;
-    
-    for (let i = 0; i<256; i++) {
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
         let square = document.createElement('div');
-        square.style.backgroundColor = 'blue';
+        square.addEventListener("mouseover", colorSquare);
+        square.style.backgroundColor = 'white';
         board.insertAdjacentElement("beforeend", square);
     };
 };
 
-function changeSize(input) {
-    genBoard(input)
-}
-
 genBoard(16);
+
+function changeSize(input) {
+    if (input >= 2 && input <= 100) {
+        genBoard(input)
+    } else {
+    console.log('Error: Input not between 2 & 100');
+    };
+};
+
+function colorSquare {
+    square.style.backgroundColor = 'black';
+};
