98353952fbde9f02af92596fd3e40a661bf42511
Nick Yaremovich <nickyaremovich@gmail.com>
2022-06-09 16:12:30 -0600

    generate 16 x 16 board using js

---
+++ main.js
@@ -0,0 +1,17 @@
+function genBoard(size) { 
+    let board = document.querySelector('.board');
+    board.style.gridTemplateColumns = `repeat(${size}), 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}), 1fr)`;
+    
+    for (let i = 0; i<256; i++) {
+        let square = document.createElement('div');
+        square.style.backgroundColor = 'blue';
+        board.insertAdjacentElement("beforeend", square);
+    };
+};
+
+function changeSize(input) {
+    genBoard(input)
+}
+
+genBoard(16);
