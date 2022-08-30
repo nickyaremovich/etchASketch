b644ba5b65055c8063d9beaf2fe97ab86d2707f0
Nick Yaremovich <nickyaremovich@gmail.com>
2022-08-30 15:31:34 -0600

    basic functionality achieved, will revisit to add style

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-
-let board = document.querySelector('.board');
-board.style.gridTemplateRows = 'repeat(16, 1fr)';
-board.style.gridTemplateColumns = 'repeat(16, 1fr)'
-let squares
-for (let i = 0; i < 256; i++) {
+function setSize() {
+    let size = prompt("Please enter a number between 1 & 100");
+    let area = size * size;
+    
+    let board = document.querySelector('.board');
+    let square = board.querySelectorAll('div');
+    
+    square.forEach((div) => div.remove());
+   
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr`;
+    
+    for (let i = 0; i < area; i++) {
     let squares = document.createElement('div');
     squares.style.backgroundColor = "#FFFFFF";
     squares.addEventListener('mouseover', () => {
         squares.style.backgroundColor = "black";
     });
     board.insertAdjacentElement("beforeend", squares);
-}
-
+    }
+}       
