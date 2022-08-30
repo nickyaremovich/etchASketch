518277cdad927ac7a3c4a2f7432b8934931c423a
Nick Yaremovich <nickyaremovich@gmail.com>
2022-08-30 14:58:25 -0600

    added board area and mouseover event listener

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-let board = document.getElementsByClassName('board');
+
+let board = document.querySelector('.board');
+board.style.gridTemplateRows = 'repeat(16, 1fr)';
+board.style.gridTemplateColumns = 'repeat(16, 1fr)'
+let squares
+for (let i = 0; i < 256; i++) {
+    let squares = document.createElement('div');
+    squares.style.backgroundColor = "#FFFFFF";
+    squares.addEventListener('mouseover', () => {
+        squares.style.backgroundColor = "black";
+    });
+    board.insertAdjacentElement("beforeend", squares);
+}
 
